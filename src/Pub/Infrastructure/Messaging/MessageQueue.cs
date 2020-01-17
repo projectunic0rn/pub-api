cb4261a981cd1ffc51641f0cea786ed6727ec0bd
Roy Moran <roy_moran1993@yahoo.com>
2020-01-16 18:20:42 -0600

    change queue logic to avoid service bus message limit exception, add logging (#62)

--- src/Pub/Infrastructure/Messaging/MessageQueue.cs
+++ src/Pub/Infrastructure/Messaging/MessageQueue.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Common.Contracts;
 using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace Infrastructure.Messaging
@@ -13,21 +14,29 @@ namespace Infrastructure.Messaging
         private IQueueClient _queueClient;
         private string _serviceBusConnectionString;
         private string _serviceBusQueueName;
+        private int _serviceBusMessageLimitByteCount;
+        private readonly ILogger _logger;
 
-        public MessageQueue(string serviceBusConnectionString, string serviceBusQueueName)
+        public MessageQueue(string serviceBusConnectionString, string serviceBusQueueName, ILogger<MessageQueue> logger)
         {
             _serviceBusConnectionString = serviceBusConnectionString;
             _serviceBusQueueName = serviceBusQueueName;
+            // Service Bus limits standard tier to message size
+            // of 256KB more info here https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-quotas
+            _serviceBusMessageLimitByteCount = 256000;
+            _logger = logger;
         }
 
         public async Task SendMessageAsync<T>(T messageObject)
         {
+            int totalMessageCount = 1;
             _queueClient = new QueueClient(_serviceBusConnectionString, _serviceBusQueueName);
             string serializedMessage = JsonConvert.SerializeObject(messageObject);
             byte[] messageContent = Encoding.UTF8.GetBytes(serializedMessage);
             Message message = new Message(messageContent);
             await _queueClient.SendAsync(message);
             await _queueClient.CloseAsync();
+            _logger.LogInformation($"Queued {totalMessageCount} of {totalMessageCount} message(s)");
             return;
         }
 
@@ -35,15 +44,35 @@ namespace Infrastructure.Messaging
         {
             _queueClient = new QueueClient(_serviceBusConnectionString, _serviceBusQueueName);
             List<Message> messages = new List<Message>();
+            long totalMessagesByteCount = 0;
+            int totalMessageCount = messageObjects.Count;
+            int messagesProcessed = 0;
             foreach (T messageObject in messageObjects)
             {
                 string serializedMessage = JsonConvert.SerializeObject(messageObject);
                 byte[] messageContent = Encoding.UTF8.GetBytes(serializedMessage);
                 Message message = new Message(messageContent);
+                totalMessagesByteCount += message.Size;
+
+                if (totalMessagesByteCount >= _serviceBusMessageLimitByteCount)
+                {
+                    // Message exceeds byte count limit so
+                    // send these messages and start new message
+                    // list.
+                    await _queueClient.SendAsync(messages);
+                    messagesProcessed += messages.Count;
+                    _logger.LogInformation($"Queued {messagesProcessed} of {totalMessageCount} message(s)");
+                    messages = new List<Message>();
+                    totalMessagesByteCount = message.Size;
+                }
+
                 messages.Add(message);
             }
+
             await _queueClient.SendAsync(messages);
             await _queueClient.CloseAsync();
+            messagesProcessed += messages.Count;
+            _logger.LogInformation($"Queued {messagesProcessed} of {totalMessageCount} message(s)");
             return;
         }
     }
