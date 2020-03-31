ff782365120a4e62a4e810f8415c99619ffdd4a4
roy <romoran1@outlook.com>
2020-03-31 15:21:02 -0500

    add pub jobs projects and tests

---
+++ src/Pub/PubJobs.IntegrationTests/WorkspaceInvitesTests/WorkspaceInviteTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.Services;
+using Xunit;
+
+namespace PubJobs.IntegrationTests.WorkspaceInvitesTests
+{
+    public class WorkspaceInviteTests
+    {
+        [Fact]
+        public async Task InviteTests_ValidateExpiredSlackInvite_ReturnsFalse()
+        {
+            //Arrange
+            SlackService _workspaceService = new SlackService();
+            string inviteLink = "https://join.slack.com/t/programmingbuddies-co/shared_invite/zt-d0ql429i-fbqV_e1iZWXErVrulp2Jng";
+            //Act
+            var result = await _workspaceService.GetInviteStatus(inviteLink);
+            //Assert
+            Assert.False(result.Valid);
+        }
+
+        [Fact]
+        public async Task InviteTests_ValidateSlackInviteNotExpired_ReturnsTrue()
+        {
+            //Arrange
+            SlackService _workspaceService = new SlackService();
+            string inviteLink = "https://join.slack.com/t/project-unic0rn/shared_invite/enQtNzUyNTA1OTgzNzE5LWJmNDYyMjQ0M2U0NmE1NGQ3NTYzYjE3MGQyOTg4ZmU1MDlhOTRlZWVhOTBmMTgxNmI2ZGRhYWExNTVmYmE0ZDI";
+            //Act
+            var result = await _workspaceService.GetInviteStatus(inviteLink);
+            //Assert
+            Assert.True(result.Valid);
+        }
+
+        [Fact]
+        public async Task InviteTests_ValidateDiscorcdInviteNotExpired_ReturnsTrue()
+        {
+            //Arrange
+            DiscordService _workspaceService = new DiscordService();
+            string inviteLink = "https://discordapp.com/invite/s7x5M4T";
+            //Act
+            var result = await _workspaceService.GetInviteStatus(inviteLink);
+            //Assert
+            Assert.True(result.Valid);
+        }
+
+        [Fact]
+        public async Task InviteTests_ValidateDiscorcdInviteExpired_ReturnsFalse()
+        {
+            //Arrange
+            DiscordService _workspaceService = new DiscordService();
+            string inviteLink = "https://discordapp.com/invite/BFydMyn";
+            //Act
+            var result = await _workspaceService.GetInviteStatus(inviteLink);
+            //Assert
+            Assert.False(result.Valid);
+        }
+    }
+}
---
+++ src/Pub/PubJobs/Jobs/HideDeadProjects.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.Contracts;
+using Common.Services;
+using Common.DTOs;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace PubJobs.Jobs
+{
+    /// <summary>
+    /// Flags projects with expired workspace invite link. If invalid, project
+    /// searchable property is set to false and project owner is notified (if
+    /// project owner exists).
+    /// </summary>
+    public class HideDeadProjects : BackgroundService
+    {
+        private readonly ILogger _logger;
+        private readonly Dictionary<string, IWorkspaceService> _workspaceServices;
+        private readonly PubService _pubService;
+        private readonly INotifier _notifier;
+
+        public HideDeadProjects(ILogger<HideDeadProjects> logger, PubService pubService, INotifier notifier)
+        {
+            _logger = logger;
+            _pubService = pubService;
+            _workspaceServices = new Dictionary<string, IWorkspaceService>();
+            _workspaceServices.Add("slack", new SlackService());
+            _workspaceServices.Add("discord", new DiscordService());
+            _notifier = notifier;
+            _logger.LogInformation($"Initialized {GetType().Name}");
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"Executing {GetType().Name}");
+                var projects = await _pubService.GetProjects();
+
+                foreach (var project in projects.Data)
+                {
+                    _workspaceServices.TryGetValue(project.CommunicationPlatform, out IWorkspaceService service);
+                    var invite = await service.GetInviteStatus(project.CommunicationPlatformUrl);
+                    if (!invite.Valid)
+                    {
+                        project.Searchable = false;
+                        await _pubService.UpdateProject(project);
+                        var projectOwner = GetProjectOwner(project);
+                        if(projectOwner == default(ProjectUserDto))
+                        {
+                            // no owner found for project
+                            continue;
+                        }
+
+                        var notificationDto = new NotificationDto(projectOwner.UserId)
+                        {
+                            NotificationObject = project
+                        };
+                        await _notifier.SendInvalidWorkspaceInviteNotificationAsync(notificationDto);
+                    }
+
+                }
+
+                _logger.LogDebug($"Found {projects.Data.Count} projects.");
+                await Task.Delay(1800000, stoppingToken);
+            }
+        }
+
+        private ProjectUserDto GetProjectOwner(ProjectDto projects)
+        {
+            List<ProjectUserDto> projectUsers = projects.ProjectUsers;
+            ProjectUserDto projectUser = projectUsers.Find(p => p.IsOwner);
+            return projectUser;
+        }
+    }
+}
