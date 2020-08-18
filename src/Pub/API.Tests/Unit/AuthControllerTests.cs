42fe390e5eed61522f13dc1c5a7b9ba4fd1dc6f4
Roy Moran <romoran1@outlook.com>
2020-08-17 22:36:28 -0500

    add watch task, test for exceptions on register

--- src/Pub/API.Tests/Unit/AuthControllerTests.cs
+++ src/Pub/API.Tests/Unit/AuthControllerTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using Xunit;
 using Common.Contracts;
@@ -6,6 +5,8 @@ using API.Controllers;
 using Moq;
 using Common.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using Domain.Exceptions;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Tests.Unit
 {
@@ -35,6 +36,64 @@ namespace API.Tests.Unit
             var objectResult = (ResponseDto<JsonWebTokenDto>)response.Value;
             // Assert
             Assert.Equal(jsonWebToken.Token, objectResult.Data.Token);
+        }
+
+        [Fact]
+        public async Task Register_CallWithMockedIAuthentication_ThrowsAndCatchesAuthenticationException()
+        {
+            // Arrange
+            var mock = new Mock<IAuthentication>();
+            var registrationDto = new RegistrationDto() {
+                Username = "roymoran",
+                Email = "roy@email.com",
+                Password = "password",
+                PasswordConfirmation = "password",
+                Locale = "en-US",
+                Timezone = "Asia/Calcutta",
+            };
+
+            string exceptionMessage = "ExceptionThrown";
+            var ex = new AuthenticationException(exceptionMessage);
+
+            mock.Setup(auth => auth.RegisterUserAsync(registrationDto)).ThrowsAsync(ex);
+            var authController = new AuthController(mock.Object);
+
+            // Act
+            var result = await authController.Register(registrationDto);
+            var response = (BadRequestObjectResult)result;
+            var objectResult = (ResponseDto<ErrorDto>)response.Value;
+
+            // Assert
+            Assert.Equal(exceptionMessage, objectResult.Data.Message);
+        }
+
+        [Fact]
+        public async Task Register_CallWithMockedIAuthentication_ThrowsAndCatchesDbUpdateException()
+        {
+            // Arrange
+            var mock = new Mock<IAuthentication>();
+            var registrationDto = new RegistrationDto() {
+                Username = "roymoran",
+                Email = "roy@email.com",
+                Password = "password",
+                PasswordConfirmation = "password",
+                Locale = "en-US",
+                Timezone = "Asia/Calcutta",
+            };
+
+            string exceptionMessage = ExceptionMessage.UniquenessConstraintViolation;
+            var ex = new DbUpdateException(exceptionMessage);
+
+            mock.Setup(auth => auth.RegisterUserAsync(registrationDto)).ThrowsAsync(ex);
+            var authController = new AuthController(mock.Object);
+
+            // Act
+            var result = await authController.Register(registrationDto);
+            var response = (BadRequestObjectResult)result;
+            var objectResult = (ResponseDto<ErrorDto>)response.Value;
+
+            // Assert
+            Assert.Equal(exceptionMessage, objectResult.Data.Message);
         }
     }
 }
