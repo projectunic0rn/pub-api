7009fbff15fafff6808f373d369fa83893ae64bc
Roy Moran <romoran1@outlook.com>
2020-08-17 21:54:43 -0500

    add register test for auth controller

---
+++ src/Pub/API.Tests/Unit/AuthControllerTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Common.Contracts;
+using API.Controllers;
+using Moq;
+using Common.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Tests.Unit
+{
+    public class AuthControllerTests
+    {
+        [Fact]
+        public async Task Register_CallWithMockedIAuthentication_ReturnsOkObjectResult()
+        {
+            // Arrange
+            var mock = new Mock<IAuthentication>();
+            var registrationDto = new RegistrationDto() {
+                Username = "roymoran",
+                Email = "roy@email.com",
+                Password = "password",
+                PasswordConfirmation = "password",
+                Locale = "en-US",
+                Timezone = "Asia/Calcutta",
+            };
+
+            var jsonWebToken = new JsonWebTokenDto("expected_token");
+            mock.Setup(auth => auth.RegisterUserAsync(registrationDto)).ReturnsAsync(jsonWebToken);
+            var authController = new AuthController(mock.Object);
+
+            // Act
+            var result = await authController.Register(registrationDto);
+            var response = (OkObjectResult)result;
+            var objectResult = (ResponseDto<JsonWebTokenDto>)response.Value;
+            // Assert
+            Assert.Equal(jsonWebToken.Token, objectResult.Data.Token);
+        }
+    }
+}
