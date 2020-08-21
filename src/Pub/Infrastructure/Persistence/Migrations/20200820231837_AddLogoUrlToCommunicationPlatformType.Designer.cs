40e7be85dba6c879e5e608eaea8dadda3cbede12
Roy Moran <roy_moran1993@yahoo.com>
2020-08-20 21:09:15 -0500

    add logo url to communication platform type table (#156)
    * add logo url to communication platform type table
    
    * add exclude migrations directory from test coverage

--- src/Pub/Infrastructure/Persistence/Migrations/20200820231837_AddLogoUrlToCommunicationPlatformType.Designer.cs
+++ src/Pub/Infrastructure/Persistence/Migrations/20200820231837_AddLogoUrlToCommunicationPlatformType.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200820231837_AddLogoUrlToCommunicationPlatformType")]
+    partial class AddLogoUrlToCommunicationPlatformType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
