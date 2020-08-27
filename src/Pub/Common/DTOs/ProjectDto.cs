846e8fb1855ce02d2c78742617aa849e80774010
Roy Moran <roy_moran1993@yahoo.com>
2020-08-27 05:30:27 -0500

    Modify dto (#167)
    * add property
    
    * bump version

--- src/Pub/Common/DTOs/ProjectDto.cs
+++ src/Pub/Common/DTOs/ProjectDto.cs
@@ -11,6 +11,7 @@ namespace Common.DTOs
         public string CommunicationPlatformUrl { get; set; }
         public string RepositoryUrl { get; set; }
         public string CommunicationPlatform { get; set; }
+        public bool LookingForMembers { get; set; }
         public bool Searchable { get; set; }
         public List<ProjectTechnologyDto> ProjectTechnologies { get; set; }
         public List<ProjectUserDto> ProjectUsers { get; set; }
