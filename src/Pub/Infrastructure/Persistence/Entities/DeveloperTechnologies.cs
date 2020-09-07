03e4a847ea46df7450013c9d11db3b55508d2e20
Roy Moran <romoran1@outlook.com>
2020-09-07 03:04:47 -0500

    modify dev recommendations to include profile link

--- src/Pub/Infrastructure/Persistence/Entities/DeveloperTechnologies.cs
+++ src/Pub/Infrastructure/Persistence/Entities/DeveloperTechnologies.cs
@@ -4,6 +4,7 @@ namespace Infrastructure.Persistence.Entities
 {
     public class DeveloperTechnologies
     {
+        public Guid UserId { get; set; }
         public string WorkspaceMemberId { get; set; }
         public string Name { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
