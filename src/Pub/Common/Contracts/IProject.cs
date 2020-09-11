810a6db53b5afef9db5b39f765a1da6a23464452
Roy Moran <roy_moran1993@yahoo.com>
2020-09-11 07:42:38 -0500

    add patch projects endpoint, add tests, modify project entity (#182)
    * add patch projects endpoint, add tests, modify project entity
    
    * bump version -> v1.0.24

--- src/Pub/Common/Contracts/IProject.cs
+++ src/Pub/Common/Contracts/IProject.cs
@@ -2,15 +2,17 @@ using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Common.DTOs;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace Common.Contracts
 {
     public interface IProject
     {
         Task<List<ProjectDto>> GetProjectsAsync(bool searchableOnly = true);
-        Task<DetailedProjectDto> GetProjectAsync(Guid id);
+        Task<DetailedProjectDto> GetProjectAsync(Guid Id);
         Task<ProjectDto> CreateProjectAsync(ProjectDto project);
-        Task<ProjectDto> UpdateProjectAsync(ProjectDto project);
-        Task DeleteProjectAsync(Guid id);
+        Task<DetailedProjectDto> UpdateProjectAsync(DetailedProjectDto project);
+        Task<DetailedProjectDto> PatchProjectAsync(Guid Id, JsonPatchDocument projectPatch);
+        Task DeleteProjectAsync(Guid Id);
     }
 }
