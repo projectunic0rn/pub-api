5f525e9c95f0791d200c95cc3d086135625dee18
Roy Moran <romoran1@outlook.com>
2020-10-23 15:36:48 -0500

    add deactived property to chat app entity

---
+++ src/Pub/Infrastructure/Persistence/Migrations/20201023202739_AddDeactivatedFlagToChatAppTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class AddDeactivatedFlagToChatAppTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deactived",
+                table: "ChatAppUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deactived",
+                table: "ChatAppUsers");
+        }
+    }
+}
