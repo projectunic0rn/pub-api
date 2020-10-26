35115b993234035d8c9c64e6ac9ab35ade81a4a7
Roy Moran <romoran1@outlook.com>
2020-10-25 19:22:47 -0500

    remove project channel id and workspace id from detailed project dto

--- src/Pub/Common/DTOs/DetailedProjectDto.cs
+++ src/Pub/Common/DTOs/DetailedProjectDto.cs
@@ -9,11 +9,9 @@ namespace Common.DTOs
         public string Name { get; set; }
         public string Description { get; set; }
         public string ExtendedMarkdownDescription { get; set; }
-        public string WorkspaceId { get; set; }
         public bool WorkspaceAppInstalled { get; set; }
         public string WorkspaceMemberName { get; set; }
         public string WorkspaceRecentMessages { get; set; }
-        public string WorkspaceProjectChannelId { get; set; }
         public string WorkspaceProjectChannelName { get; set; }
         public string RepositoryUrl { get; set; }
         public string CommunicationPlatformUrl { get; set; }
