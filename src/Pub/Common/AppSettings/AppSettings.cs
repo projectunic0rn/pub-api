16bc15410a4cc61f045d3db2666038c07c3990f1
Roy Moran <romoran1@outlook.com>
2020-10-29 19:52:20 -0500

    map messages string to list

--- src/Pub/Common/AppSettings/AppSettings.cs
+++ src/Pub/Common/AppSettings/AppSettings.cs
@@ -8,7 +8,7 @@ namespace Common.AppSettings
     public static class AppSettings
     {
         // API
-        public static string ApiV1 { get; } = "v1.0.42";
+        public static string ApiV1 { get; } = "v1.0.43";
         public static string ApiName { get; } = "Pub";
         public static string Env { get; set; } = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", EnvironmentVariableTarget.Process);
         public static string JwtAudience { get; set; } = Environment.GetEnvironmentVariable("JwtAudience", EnvironmentVariableTarget.Process);
