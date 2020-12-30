82e0fd4e6c83b20b93fdb4d654ce01c154db90e3
Roy Moran <roy_moran1993@yahoo.com>
2020-12-30 14:20:43 -0600

    add startup log (#221)

--- src/Pub/PubJobs/Startup.cs
+++ src/Pub/PubJobs/Startup.cs
@@ -49,6 +49,7 @@ namespace PubJobs
 
             services.AddSingleton<IMessageQueue>((container) =>
             {
+                container.GetRequiredService<ILogger<Startup>>().LogError("PubJobs v0.0.1");
                 var logger = container.GetRequiredService<ILogger<MessageQueue>>();
                 return new MessageQueue(_settings.ServiceBusConnectionString, _settings.ServiceBusQueueName, logger);
             });
